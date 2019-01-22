e1b3687ded7db0aed84d969a70c6a7981f42f5aa
Michael Ferrell <michaelf@conversica.com>
2019-01-21 19:06:32 -0800

    add else if in date format

--- src/js/helpers/formatTime.js
+++ src/js/helpers/formatTime.js
@@ -3,7 +3,17 @@ import { getHours, getMinutes } from "date-fns"
 import { TIME_CONVERSION } from "./../constants/"
 
 export const formatTime = date_str => {
-  const date = new Date(date_str)
+  let date
+  // this method is used in multiple scenarios
+  // string when called be NBAGameModel
+  // object in other places
+  // future fix: normalize dates
+  if (typeof date_str === "string") {
+    let d = date_str.split(/[- T :]/)
+    date = new Date(d[0], parseInt(d[1]) - 1, d[2], d[3], d[4], d[5])
+  } else if (typeof date_str === "object") {
+    date = new Date(date_str)
+  }
   let minutes = getMinutes(date)
   minutes = (minutes < 10 ? "0" : "") + minutes
   const east_time_diff = 3
